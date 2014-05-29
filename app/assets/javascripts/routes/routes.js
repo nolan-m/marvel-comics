a12dfccd9d252078cf4d42e441af282a36dd0a32
Nolan Miller <namiller2@gmail.com>
2014-05-28 20:44:53 -0700

    more heroku config

--- app/assets/javascripts/routes/routes.js
+++ app/assets/javascripts/routes/routes.js
@@ -27,11 +27,11 @@ ComicTracker.IndexRoute = Ember.Route.extend({
 ComicTracker.WeekRoute = Ember.Route.extend({
   model: function(params) {
     if (params.week_id === 'current') {
-      var week = this.store.find('week', 2);
+      var week = this.store.find('week', 5);
     } else if (params.week_id === 'next') {
-      var week = this.store.find('week', 3);
+      var week = this.store.find('week', 6);
     } else {
-      var week = this.store.find('week', 1);
+      var week = this.store.find('week', 4);
     }
     return week;
   }
